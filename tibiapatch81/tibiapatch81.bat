8fa85db156a54c1cbd897281dc548d6e96e4db91
evretibia <evretibia@cc901e99-3b3f-0410-afbc-77a0fa429cc7>
2008-03-27 04:02:30 +0000

    Last updated on March 26th, 2008

---
+++ tibiapatch81/tibiapatch81.bat
@@ -0,0 +1,3 @@
+REM http://www.mingw.org/
+c:\mingw\bin\gcc.exe "c:\cpp\tibiapatch81\tibiapatch81.c" -Wall -O2 -lcomdlg32 -Wl,-subsystem,windows -o c:\cpp\tibiapatch81\release\tibiapatch81.exe
+PAUSE
