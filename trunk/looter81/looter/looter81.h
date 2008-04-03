5becb6f743deee335229cd9aa7fe634114542cbc
evretibia <evretibia@cc901e99-3b3f-0410-afbc-77a0fa429cc7>
2008-04-03 21:34:38 +0000

    Last updated on April 3rd, 2008
    cod4levelup
    fasthands81
    ini
    levelspy81
    looter81
    tibia
    tibiapatch81
    tibiatest81
    trainer

---
+++ trunk/looter81/looter/looter81.h
@@ -0,0 +1,33 @@
+#ifndef _LOOTER81_H_
+#define _LOOTER81_H_
+
+// window dimensions
+#define WINDOW_WIDTH	320
+#define WINDOW_HEIGHT	240
+
+// tray notification message
+#define WM_SHELLNOTIFY	WM_USER+5
+
+// application icon, tray menu and tray icon
+#define ID_ICON_LOOTER	101
+#define ID_MENU_LOOTER	102
+#define ID_TRAY_LOOTER	103
+
+// timers
+#define ID_TIMER_HOTKEYS	1001
+#define ID_TIMER_LOOT		1002
+#define ID_TIMER_STACK		1003
+#define ID_TIMER_EAT		1004
+
+// menu items
+#define ID_MENU_NULL		10001
+#define ID_MENU_INI			10002
+#define ID_MENU_LOOT		10003
+#define ID_MENU_STACK		10004
+#define ID_MENU_EAT			10005
+#define ID_MENU_SEE_ID		10006
+#define ID_MENU_ABOUT		10007
+#define ID_MENU_HOMEPAGE	10008
+#define ID_MENU_EXIT		10009
+
+#endif // #ifndef _LOOTER81_H_
