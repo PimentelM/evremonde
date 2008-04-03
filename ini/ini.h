600abc1eae1e491457eba256292b6dd742473e79
evretibia <evretibia@cc901e99-3b3f-0410-afbc-77a0fa429cc7>
2008-04-03 21:34:15 +0000

    Last updated on April 3rd, 2008
    cod4levelup
    fasthands81
    ini
    levelspy81
    looter81
    tibia
    tibiapatch81
    tibiatest81
    trainer

---
+++ ini/ini.h
@@ -0,0 +1,22 @@
+#ifndef _INI_H_
+#define _INI_H_
+
+#include <vector>
+#include <string>
+#include <fstream>
+
+#include <windows.h>
+
+class CIni
+{
+  public:
+	// constructor and deconstructor
+	CIni();
+	~CIni();
+
+	// read ini
+	std::vector<std::string> getSections(std::string fileName);
+	int readInteger(std::string fileName, std::string section, std::string key);
+};
+
+#endif // #ifndef _INI_H_
